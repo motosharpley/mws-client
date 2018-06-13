4afc1680be0ac5a46f9eea042307116003260b79
Bryan Sharpley <motosharpley@gmail.com>
2018-06-13 16:40:57 -0500

    update port, fix fetchrestaurants

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,22 +8,25 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    // return `http://localhost:${port}/data/restaurants.json`;
+    return `http://localhost:${port}/restaurants`;
     // return '/data/restaurants.json';
     // return 'https://motosharpley.github.io/mws-stage-1/data/restaurants.json';
   }
-
+  
+  
+ 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
     let xhr = new XMLHttpRequest();
+    
     xhr.open('GET', DBHelper.DATABASE_URL);
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+        const restaurants = JSON.parse(xhr.responseText);
         callback(null, restaurants);
       } else { // Oops!. Got an error from server.
         const error = (`Request failed. Returned status of ${xhr.status}`);
