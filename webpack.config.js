6368347d9850ad6c8272add5668605db4dd9637e
Bryan Sharpley <motosharpley@gmail.com>
2018-06-16 22:23:27 -0500

    add webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src',
+  entry: {
+    app: './src/index.js',
+    restaurants: './src/js/restaurants.js',
+    dbhelper: './src/js/dbhelper.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
