9e5698b70eae644cbb136f70340294724b504d87
Bryan Sharpley <motosharpley@gmail.com>
2018-06-19 10:37:17 -0500

    add polyfills and rest.html & js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
+    polyfills: './src/js/polyfills.js',
     app: './src/index.js'
   },
   devtool: 'inline-source-map',
@@ -19,9 +20,15 @@ module.exports = {
       title: 'Restaurant Reviews',
       template: 'src/index.html',
     }),
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Details',
+      template: 'src/restaurant.html',
+      filename: 'restaurant.html'
+    }),
     new CopyWebpackPlugin([
       {from: './src/js/dbhelper.js', to: './js'},
       {from: './src/js/main.js', to: './js'},
+      {from: './src/js/restaurant_info.js', to: './js'},
       {from: './src/sw.js'},
       {from: 'src/img', to: './img'},
       {from: './src/manifest.json'}
@@ -30,7 +37,7 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   output: {
-    filename: './js/[name].bundle.js',
+    filename: '.[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -41,6 +48,11 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
