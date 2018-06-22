54b67bab83de36ba3cad156325b9bf3dc694d6f2
Bryan Sharpley <motosharpley@gmail.com>
2018-06-22 13:04:58 -0500

    set up base file copy dist structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
 
+//  --- Static Assets ---
 gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
@@ -14,5 +15,28 @@ gulp.task('css', function() {
     .pipe(gulp.dest('dist/css'))
 })
 
+gulp.task('img', function() {
+  return gulp.src('src/img/*')
+    .pipe(gulp.dest('dist/img'))
+})
 
-gulp.task('default', ['html', 'css'])
+gulp.task('manifest', function() {
+  return gulp.src('src/manifest.json')
+    .pipe(gulp.dest('dist'))
+})
+
+// --- JS Assets ---
+gulp.task('sw', function() {
+  return gulp.src('src/sw.js')
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('js', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist/js'))
+})
+
+
+
+
+gulp.task('default', ['html', 'css', 'img', 'manifest', 'sw', 'js'])
