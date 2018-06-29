69525010471ab12d0a6df3ef8ad513a369de07bc
Bryan Sharpley <motosharpley@gmail.com>
2018-06-28 22:01:52 -0500

    clean up and fix h2 - h3

--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -48,24 +48,6 @@ initMap = () => {
   });
 }  
 
-// /**
-//  * Initialize Google map, called from HTML.
-//  */
-// window.initMap = () => {
-//   fetchRestaurantFromURL((error, restaurant) => {
-//     if (error) { // Got an error!
-//       console.error(error);
-//     } else {
-//       self.map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 16,
-//         center: restaurant.latlng,
-//         scrollwheel: false
-//       });
-//       fillBreadcrumb();
-//       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-//     }
-//   });
-// }
 
 /**
  * Get current restaurant from page URL.
@@ -145,7 +127,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
